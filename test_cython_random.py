76b798ad68398ab8c831aabae765e02ebeae6cdc
Ivan Krukov <ivan.o.kryukov@gmail.com>
2019-05-27 14:00:47 -0600

    fixed test

--- test_cython_random.py
+++ test_cython_random.py
@@ -1,34 +1,30 @@
-import pyximport
 import numpy as np
-pyximport.install(setup_args={'include_dirs': np.get_include()})
 import cython_random
 
-
-cython_random.py_seed_random(1234)
-
-for i in range(1000):
-    cython_random.py_rand_int()
-
-assert ( cython_random.py_rand_int() == 15529542738993802093 )
-
+cython_random.seed_random(1234)
 
 for i in range(1000):
-    cython_random.py_rand_int()
+    cython_random.genrand64()
 
-assert ( cython_random.py_rand_int() == 13925894414736521713 )
-
-
-for i in range(1000):
-    cython_random.py_rand_int()
-
-assert ( np.abs(0.3677696173800852341706502 - cython_random.py_uniform_rv() )< 1E-10 )
+assert (cython_random.genrand64() == 15529542738993802093)
 
 for i in range(1000):
-    cython_random.py_rand_int()
+    cython_random.genrand64()
 
-assert ( np.abs( 0.3083103800285806328496108 - cython_random.py_uniform_rv() ) < 1E-10 )
+assert (cython_random.genrand64() == 13925894414736521713)
 
+for i in range(1000):
+    cython_random.genrand64()
 
-print('If you get to this message with no errors, you have installed correctly :-)')
+assert (np.abs(0.3677696173800852341706502 - cython_random.uniform_rv()) <
+        1E-10)
 
+for i in range(1000):
+    cython_random.genrand64()
 
+assert (np.abs(0.3083103800285806328496108 - cython_random.uniform_rv()) <
+        1E-10)
+
+print(
+    'If you get to this message with no errors, you have installed correctly :-)'
+)
