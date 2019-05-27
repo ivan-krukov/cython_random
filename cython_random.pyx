088bff5f9b2cd6bcf699bfc431816d6546e40071
Ivan Krukov <ivan.o.kryukov@gmail.com>
2019-05-27 14:00:00 -0600

    cpdef instead of py_ functions, cleanded up imports

--- cython_random.pyx
+++ cython_random.pyx
@@ -1,16 +1,12 @@
+# cython: language_level=3
 # cython: boundscheck=False
 # cython: cdivision=True
 # cython: wraparound=False
 
-cimport cython
-from libc.stdlib cimport rand, srand
-from libc.math cimport log, sqrt, cos, round
+from libc.math cimport log, sqrt, cos
 
 cimport numpy as np
 import time
-
-cdef extern from "stdlib.h":
-    cdef int RAND_MAX # Need to this to normalize appropriately to uniform dist
 
 # MT Stuff
 
@@ -38,7 +34,7 @@ cdef mt_seed(unsigned long long seed):
     mti = NN
 
 
-cdef unsigned long long genrand64():
+cpdef unsigned long long genrand64():
     cdef int i
     cdef unsigned long long x
     global mag01
@@ -71,13 +67,10 @@ cdef unsigned long long genrand64():
 
     return x
 
-def py_rand_int():
-    return genrand64()
-
 # Functions
 
 # Seed the random number generator
-cdef seed_random(unsigned long long seed):
+cpdef seed_random(unsigned long long seed):
     """
     Seed the C random number generator with the current system time.
     :return: none
@@ -87,12 +80,8 @@ cdef seed_random(unsigned long long seed):
     else:
         mt_seed(seed)
 
-def py_seed_random(unsigned long long seed = 0):
-    seed_random(seed)
 
-
-
-cdef double exponential_rv(double Lambda):
+cpdef double exponential_rv(double Lambda):
     """
     Generate an exponentially distributed random number
     :param Lambda: (double) the rate parameter of the distribution
@@ -100,26 +89,18 @@ cdef double exponential_rv(double Lambda):
     """
     return -1.0/Lambda* log( uniform_rv() )
 
-def py_exponential_rv(double Lambda):
-    return exponential_rv(Lambda)
 
 
-
-cdef double uniform_rv():
+cpdef double uniform_rv():
     """
     Generate a uniform random variable in [0,1]
     :return: (double) a random uniform number in [0,1]
     """
     return (genrand64() >> 11) * (1.0/9007199254740991.0)
 
-def py_uniform_rv():
-    return uniform_rv()
 
 
-
-
-
-cdef double normal_rv(double mean, double std):
+cpdef double normal_rv(double mean, double std):
     """
     Generate a normal random variable
     :param mean: (double) the mean
@@ -133,13 +114,8 @@ cdef double normal_rv(double mean, double std):
     theta = 2*3.141592653589793238462643383279502884*v
     return R*cos(theta)*std + mean
 
-def py_normal_rv(double mean, double std):
-    return normal_rv(mean,std)
 
-
-
-
-cdef double gamma_rv(double k, double theta):
+cpdef double gamma_rv(double k, double theta):
     """
     Generate a gamma random variable with parameters (k, theta). The mean is k*theta. Variance k*theta^2. Can think of
      this as adding together k independent exponentials each with mean theta.
@@ -160,13 +136,7 @@ cdef double gamma_rv(double k, double theta):
             return d*v*theta
 
 
-def py_gamma_rv(double k, double theta):
-    return gamma_rv(k,theta)
-
-
-
-
-cdef double erlang_rv(double k , double theta):
+cpdef double erlang_rv(double k , double theta):
     """
     Generate an Erlang random variable with k independent exponential steps each with mean theta. The mean is k*theta
      and the variance is k*theta^2
@@ -181,14 +151,8 @@ cdef double erlang_rv(double k , double theta):
         answer += -1.0 * theta * log( uniform_rv() )
     return answer
 
-def py_erlang_rv(double k, double theta):
-    return erlang_rv(k,theta)
 
-
-
-
-
-cdef int sample_discrete(int choices, double* data, double Lambda):
+cpdef int sample_discrete(int choices, double[:] data, double Lambda):
     """
     Sample from a discrete set of options according to some un-normalized probability weights.
     :param choices: (int) the number of possible choices. should be positive.
@@ -205,14 +169,11 @@ cdef int sample_discrete(int choices, double* data, double Lambda):
         i += 1
     return i - 1
 
-def py_sample_discrete(int choices, np.ndarray[np.double_t,ndim=1] data, double Lambda):
-    return sample_discrete(choices, <double*> (data.data), Lambda)
+# def py_sample_discrete(int choices, np.ndarray[np.double_t,ndim=1] data, double Lambda):
+    # return sample_discrete(choices, <double*> (data.data), Lambda)
 
 
-
-
-
-cdef double array_sum(double* data, int length):
+cpdef double array_sum(double[:] data, int length):
     """
     Sum an array of floating point numbers
     :param data: (double *) pointer to the array of numbers (must have len(data) >= length)
@@ -226,12 +187,12 @@ cdef double array_sum(double* data, int length):
     return answer
 
 
-def py_array_sum(np.ndarray[np.double_t,ndim=1] data, int length):
-    return array_sum(<double*> data.data, length)
+# def py_array_sum(np.ndarray[np.double_t,ndim=1] data, int length):
+    # return array_sum(<double*> data.data, length)
 
 
 
-cdef unsigned binom_rnd(unsigned n, double p):
+cpdef unsigned binom_rnd(unsigned n, double p):
     """
     Generate a binomial random number selected from binom(n,p)
     :param n: (unsigned) the population size, n>= 1
@@ -246,11 +207,11 @@ cdef unsigned binom_rnd(unsigned n, double p):
 
     return answer
 
-def py_binom_rnd(unsigned n, double p):
-    return binom_rnd(n,p)
+# def py_binom_rnd(unsigned n, double p):
+#     return binom_rnd(n,p)
 
 
-cdef unsigned binom_rnd_f(double N, double p):
+cpdef unsigned binom_rnd_f(double N, double p):
     """
     Generate a binomial random number selected from binom(N,p), N >= 1
     :param N: (double) will be rounded to nearest integer
@@ -266,12 +227,12 @@ cdef unsigned binom_rnd_f(double N, double p):
 
     return answer
 
-def py_binom_rnd_f(double N, double p):
-    return  binom_rnd_f( N,  p)
+# def py_binom_rnd_f(double N, double p):
+#     return  binom_rnd_f( N,  p)
 
 
 
-cdef unsigned approx_binom_rnd(unsigned n, double p):
+cpdef unsigned approx_binom_rnd(unsigned n, double p):
     """
     Generate an approximately binomial random variable using the normal approximation.
     :param n: (unsigned) The population size n >= 1, but should be at least 20 for good approximation.
@@ -280,13 +241,13 @@ cdef unsigned approx_binom_rnd(unsigned n, double p):
     """
     return int( normal_rv( n*p , sqrt(n*p*(1-p))  ) + 0.5 )
 
-def py_approx_binom_rnd(unsigned n, double p):
-    return approx_binom_rnd(n,p)
+# def py_approx_binom_rnd(unsigned n, double p):
+#     return approx_binom_rnd(n,p)
 
 
 
 
-cdef unsigned approx_binom_rnd_f(double n, double p):
+cpdef unsigned approx_binom_rnd_f(double n, double p):
     """
     Generate an approximately binomial random variable using the normal approximation.
     :param n: (double) The population size n >= 1, but should be at least 20 for good approximation.
@@ -295,5 +256,5 @@ cdef unsigned approx_binom_rnd_f(double n, double p):
     """
     return int( normal_rv( n*p , sqrt(n*p*(1-p))  ) + 0.5 )
 
-def py_approx_binom_rnd_f(double n, double p):
-    return approx_binom_rnd_f(n,p)
+# def py_approx_binom_rnd_f(double n, double p):
+#     return approx_binom_rnd_f(n,p)
